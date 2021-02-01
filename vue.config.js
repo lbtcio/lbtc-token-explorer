9cec8b7b2250864391d7daf5cb992b660ad7ef09
WuDing <44403890+KingWuDing@users.noreply.github.com>
2021-02-01 11:48:06 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,24 @@ module.exports = {
 
   assetsDir: undefined,
   
+  
   baseUrl: undefined,
+  
   
   outputDir: undefined,
   
+  
   runtimeCompiler: undefined,
+  
   
   productionSourceMap: undefined,
   
+  
   parallel: undefined,
   
+  
   css: undefined
+  
   
   
 }
